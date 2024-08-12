b527faaefad86423299db05c34e6f785fdc316ed
Malik <62953363+maliksvd@users.noreply.github.com>
2024-08-12 12:03:51 -0400

    feat: move the form to an Drawer

--- src/app/components/Todo/Form.tsx
+++ src/app/components/Todo/Form.tsx
@@ -1,5 +1,9 @@
 'use client'
 
+// react
+
+// framer-motion
+
 // zod & react-hook-form
 import { zodResolver } from '@hookform/resolvers/zod'
 import { SubmitHandler, useForm } from 'react-hook-form'
@@ -7,15 +11,13 @@ import { z } from 'zod'
 
 // shadcn-ui
 import { Button } from '@/components/ui/button'
-import { Card } from '@/components/ui/card'
+import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from '@/components/ui/drawer'
 import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
-import { toast } from '@/components/ui/use-toast'
 
 // icons
 import { RocketIcon } from '@radix-ui/react-icons'
-
 // types
 interface TodoFormProps {
   addTodo: (text: string, category: string) => void
@@ -43,62 +45,78 @@ export default function TodoForm({ addTodo }: TodoFormProps) {
     console.log('Submitting form with values:', values)
     try {
       addTodo(values.text, values.category)
-      toast({
-        title: 'Todo added successfully',
-      })
     } catch (error) {
       console.error('Error adding todo:', error)
-      toast({
-        title: 'Error adding todo',
-        variant: 'destructive',
-      })
     }
   }
 
   return (
-    <Card className="p-4 w-full">
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col space-y-4">
-          <FormField
-            control={form.control}
-            name="text"
-            render={({ field }) => (
-              <FormItem>
-                <Input {...field} placeholder="add a new todo" className="w-full border p-2 mr-2" />
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <div className="flex space-x-4 w-full">
-            <FormField
-              control={form.control}
-              name="category"
-              render={({ field }) => (
-                <FormItem className="w-full">
-                  <Select onValueChange={field.onChange} defaultValue={field.value}>
-                    <FormControl>
-                      <SelectTrigger className="w-[180px]">
-                        <SelectValue placeholder="select a category" />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      <SelectItem value="personal">personal</SelectItem>
-                      <SelectItem value="work">work</SelectItem>
-                      <SelectItem value="travel">travel</SelectItem>
-                      <SelectItem value="shopping">shopping</SelectItem>
-                    </SelectContent>
-                  </Select>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit" variant="outline" className="w-1/2 font-mono">
-              <RocketIcon className="h-4 w-4 mr-2" />
-              add
-            </Button>
-          </div>
-        </form>
-      </Form>
-    </Card>
+    <Drawer>
+      <DrawerTrigger className="bottom-0 left-0 right-0 flex justify-center pb-4 z-50 overflow-hidden fixed">
+        <Button>add todo</Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Add a new todo</DrawerTitle>
+          <DrawerDescription>You can add a new todo by entering a text and selecting a category.</DrawerDescription>
+        </DrawerHeader>
+        <DrawerFooter>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col space-y-4">
+              <FormField
+                control={form.control}
+                name="text"
+                render={({ field }) => (
+                  <FormItem>
+                    <Input
+                      {...field}
+                      placeholder="add a new todo"
+                      className="w-full border p-2 mr-2"
+                      onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                          e.preventDefault()
+                          form.handleSubmit(onSubmit)()
+                        }
+                      }}
+                    />{' '}
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex space-x-4 w-full">
+                <FormField
+                  control={form.control}
+                  name="category"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <Select onValueChange={field.onChange} defaultValue={field.value}>
+                        <FormControl>
+                          <SelectTrigger className="w-[180px]">
+                            <SelectValue placeholder="select a category" />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          <SelectItem value="personal">personal</SelectItem>
+                          <SelectItem value="work">work</SelectItem>
+                          <SelectItem value="travel">travel</SelectItem>
+                          <SelectItem value="shopping">shopping</SelectItem>
+                        </SelectContent>
+                      </Select>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <DrawerClose className="w-1/2 font-mono">
+                  <Button type="submit" className="font-mono">
+                    <RocketIcon className="h-4 w-4 mr-2" />
+                    add
+                  </Button>
+                </DrawerClose>
+              </div>
+            </form>
+          </Form>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
   )
 }
