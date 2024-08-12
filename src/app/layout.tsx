8c1912527d35db2c00c07b67bac35bfcdbf18655
Malik <62953363+maliksvd@users.noreply.github.com>
2024-08-12 00:10:58 -0400

    feat: add umani

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className + ' font-mono'}>{children}</body>
+      <script defer src="https://cloud.umami.is/script.js" data-website-id="29328871-573e-42b5-92d2-fd8d306d9a4d"></script>
     </html>
   )
 }
